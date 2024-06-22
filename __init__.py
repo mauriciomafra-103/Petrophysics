93b721d73f68c93edd5bbfdf5d2fbb38dc0c452d
Maurício Mafra <114196060+mauriciomafra-103@users.noreply.github.com>
2024-06-22 10:20:20 -0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 touch __init__.py
 
-from .regressao import regressao_componentes
-from .visualizacao import visualizar_componentes_t2
+from .preprocessamento import TratamentoDadosLaboratorio
+from .regressao import RegressaoSDR
+from .visualizacao import VisualizarPredicoesPermeabilidade
