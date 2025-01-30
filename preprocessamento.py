865872de47ef11b355d4c88f83fcc605d614c9e7
Maurício Mafra <114196060+mauriciomafra-103@users.noreply.github.com>
2025-01-30 09:40:23 -0300

    preprocessamento.py

--- preprocessamento.py
+++ preprocessamento.py
@@ -8,17 +8,18 @@ import numpy as np
 # Bibliotecas para Processamento RMN
 from sklearn import preprocessing
 from sklearn.preprocessing import MaxAbsScaler, OneHotEncoder
+from sklearn.metrics import mean_squared_error
 
 
 def ObtencaoDadosNiumag(Diretorio_pasta, Arquivo_niumag, Inicio_conversao, Pontos_inversao,
                         Relaxacao = False, Distribuicao = False, T2_niumag = False, Erro = False,
                         Amplitudes = False, Poco = False, N_poco_i = 4, N_poco_f = 0, N_amp = 3):
-                          
+
 
     """
       Esta função trata o dados brutos de RMN que o software da Niumag exporta retornando um pandas.DataFrame com as informações
       relevantes e com aspecto mais legível.
-  
+
       Args:
           Diretorio_pasta (str): O caminho do diretório onde está o arquivo excel exportado pelo software Niumag.
           Arquivo_niumag (str.xlsx): Nome do arquivo que está dentro do diretório e que é um arquivo tipo Excel.
@@ -33,21 +34,21 @@ def ObtencaoDadosNiumag(Diretorio_pasta, Arquivo_niumag, Inicio_conversao, Ponto
           N_poco_i (int): Identificação do começo do nome do poço.
           N_poco_f (int): Identificação do final do nome do poço.
           N_amp (int): Identificação da quantidade de amplitudes desejadas.
-  
+
       Returns:
-          pandas.DataFrame: Retorna um DataFrame com dados processados do Excel exportado pelo Software Niumag. 
-  
+          pandas.DataFrame: Retorna um DataFrame com dados processados do Excel exportado pelo Software Niumag.
+
       Exemplos de Uso:
           Caso o usuário tenha um arquivo no estilo exportado pela Niumag que informa a amostra (no nome da amostra informa o nome do poço) essa função retornará os dados
           já específico para o processamento necessário.
       """
-  
+
     niumag = str(Diretorio_pasta) + str(Arquivo_niumag)                           # Pasta do arquivp
     dados_niumag = pd.read_excel(niumag).drop('File Name', axis=1)                # Dataframe dos Dados da Niumag
-  
+
     inicio = Inicio_conversao-2                                                   # Linha que se inicia os dados de da inversão
     final = inicio+Pontos_inversao                                                # Linha final da Inversão
-  
+
     amostras = []
     poc = []
     tempo_relaxacao = []
@@ -59,17 +60,17 @@ def ObtencaoDadosNiumag(Diretorio_pasta, Arquivo_niumag, Inicio_conversao, Ponto
     t2av_niumag = []
     fitting_erro = []
     amplitudes = []
-  
+
     for i in np.arange(int(len(dados_niumag.columns)/7)):
       df = dados_niumag.T.reset_index().drop('index', axis = 1).T
-  
+
       nome = dados_niumag.columns[i*7][:9]
       amostras.append(nome)
-  
+
       if Poco == True:
         p = nome[N_poco_f:N_poco]
         poc.append(p)
-  
+
       if Relaxacao == True:
         time = df[i*7][13:]
         sinal_niumag = df[i*7+1][13:]
@@ -77,67 +78,67 @@ def ObtencaoDadosNiumag(Diretorio_pasta, Arquivo_niumag, Inicio_conversao, Ponto
         tempo_relaxacao.append(time)
         amplitude_sinal_niumag.append(sinal_niumag)
         amplitude_sinal_fitting.append(sinal_fiting)
-  
+
       if Distribuicao == True:
         tempo = df[i*7+3][inicio:final]
         dist = df[i*7+4][inicio:final]
         tempo_distribuicao.append(tempo)
         distribuicao_t2.append(dist)
-  
+
       if T2_niumag == True:
         gm = float(df[i*7+2][1][7:-4])
         av = float(df[i*7+2][2][7:-4])
         t2gm_niumag.append(gm)
         t2av_niumag.append(av)
-  
+
       if Erro == True:
         fit_erro = float(df[i*7][0][-5:])
         fitting_erro.append(fit_erro)
-  
+
       if Amplitudes == True:
         amp = df[i*7+2][5:10].fillna(0).nlargest(N_amp)
         amplitudes.append(amp)
-  
+
     df = pd.DataFrame({'Amostra': amostras})
-  
+
     if Poco == True:
       df['Poço'] = poc
-  
+
     if Relaxacao == True:
       df['Tempo Relaxacao'] = pd.Series(tempo_relaxacao)
       df['Amplitude Relaxacao'] = pd.Series(amplitude_sinal_niumag)
       df['Amplitude Relaxacao Fitting'] = pd.Series(amplitude_sinal_fitting)
-  
+
     if Distribuicao == True:
       df['Tempo Distribuicao'] = pd.Series(tempo_distribuicao)
       df['Distribuicao T2'] = pd.Series(distribuicao_t2)
-  
+
     if T2_niumag == True:
       df['T2 Geometrico Niumag'] = t2gm_niumag
       df['T2 Medio Niumag'] = t2av_niumag
-  
+
     if Erro == True:
       df['Fitting Error'] = fitting_erro
-  
+
     if Amplitudes == True:
       df['Amplitudes'] = amplitudes
-  
+
     df = df.sort_values(by = 'Amostra')
-  
+
     return df
 
 
 
 def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
-                       Porosidade_i = False, T2_log = False, Componentes_t2 = False,
-                       Fator_Cimentacao = False, V_artifical = 1.3, V_geral = 2.0, 
+                       Porosidade_i = False, poro_i = 'Porosidade RMN', T2_log = False, Componentes_t2 = False,
+                       Fator_Cimentacao = False, V_artifical = 1.3, V_geral = 2.0,
                        Fracoes_T2Han = False, Fracoes_T2Ge = False, Localizacao = False,
                        Parametros_lab = ['Permeabilidade Gas', 'Porosidade Gas', 'Porosidade RMN'],
                        Geometria = False, EPSG = 4326, Conversao = False, N_Conversao = 32724,
-                       BVIFFI = False, Fator_Formacao = False, Litofacie = False,                       
+                       BVIFFI = False, tempo = 'Arenito', Fator_Formacao = False, Litofacie = False,
                        Amplitude = False, Dados_porosidade_Transverso = False, N_transverso = 128):
     """
-    Esta função trata mescla os dados já processados de RMN (como processado pela função anterior e que tenha informações da distribuição de tamanho de poros)
+    Esta função trata mesclar os dados já processados de RMN (como processado pela função anterior e que tenha informações da distribuição de tamanho de poros)
     com os dados laboratoriais, que contenham dados de porosidade a gás e de RMN, permeabilidade a gas, litofácies das amostras.
 
     Args:
@@ -145,6 +146,7 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
         Arquivo_laboratorio (str): Nome do arquivo contendo os dados do laboratório em excel.
         Dados_niumag (pandas.DataFrame): DataFrame com as informações selecionadas da distribuição de tamanho de poros.
         Porosidade_i (bool): Caso o usuário queira a transformação do sinal de RMN em porosidade RMN.
+        poro_i (str): Nome da coluna com a porosidade que o usuário deseja normalizar o sinal de amplitude da RMN.
         T2_log (bool): Caso o usuário queira calcular o T2_lm proposto por Kenyon et al (1988). OBS: Não está pronto.
         Componentes_t2 (bool): Caso o usuário tenha as componentes que ajustam a curva de relaxação T2.
         Fator_Cimentacao (bool): Caso o usuário queira obter o fator de cimentação.
@@ -159,29 +161,30 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
         Conversao (bool): Caso o usuário queira converter os o sistema de coordenadas dos dados lab em outro.
         N_Conversao (int): Sistema de coordenadas da European Petroleum Survey Group que o usuário deseja converter a geometrias das amostras. Obs: O formato de conversão padrão é o WTF24M.
         BVIFFI (bool): Caso o usuário queira retornar as frações da modelagem proposta por Coates et al (1999).
+        tempo (str): Informações do modelo de tempo que o usuário deseja retornar.
         Fator_Formacao (bool): Caso o usuário queira obter o fator de formação.
-        Litofacie (bool): Caso o usuário tenha nos dados do laboratório informações sobre as litofácies. 
+        Litofacie (bool): Caso o usuário tenha nos dados do laboratório informações sobre as litofácies.
         Amplitude (bool): Caso o usuário queira transformar a lista com os valores da Amplitude do sinal de Relaxação em colunas
         Dados_porosidade_Transverso (bool): Caso o usuário queira transformar a lista com os valores da Distribuição de Tamanho de poros em colunas.
         N_transverso (int): Quantidade de pontos da inversão da curva de relaxação T2.
 
     Returns:
-        pandas.DataFrame: Retorna um DataFrame com dados processados do Excel exportado pelo usuário com os dados do laboratório mesclado com os dados da Niumag. 
+        pandas.DataFrame: Retorna um DataFrame com dados processados do Excel exportado pelo usuário com os dados do laboratório mesclado com os dados da Niumag.
 
     Exemplos de Uso:
         Caso o usuário tenha um arquivo .xlsx com dados de laboratório e um pandas.DataFrame com dados de Distribuição de Tamanho de Poros
         essa função retornará os dados mesclados e prontos para regressões ou visualizações no formato pandas.DataFrame.
     """
 
-    laboratorio = str(Diretorio_pasta) + str(Arquivo_laboratorio)
     dados_niumag = Dados_niumag
-    dados_lab = pd.read_excel(laboratorio)
+    dados_lab = pd.read_excel(Diretorio_pasta + Arquivo_laboratorio)
+    dados_lab['Amostra'] = dados_lab['Amostra'].astype(str)
     dados_lab = dados_lab.sort_values(by = 'Amostra')
-  
+
     tempo_distribuicao = dados_niumag['Tempo Distribuicao']
     distribuicao_t2 = dados_niumag['Distribuicao T2']
-  
-    porosidade_i = []
+
+    porosi_i = []
     media_ponderada_log = []
     s1h = []
     s2h = []
@@ -195,9 +198,10 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
     A1 = []
     A2 = []
     A3 = []
-  
-    df = pd.concat([dados_niumag, dados_lab[Parametros_lab]], axis = 1)
-  
+
+    df_niumag_sa = dados_niumag.drop('Amostra', axis = 1)
+    df = pd.concat([dados_lab[Parametros_lab], df_niumag_sa, ], axis = 1)
+
     if Litofacie == True:
       codi_lab = preprocessing.LabelEncoder()
       categoria_lito = codi_lab.fit_transform(dados_lab['Litofacies'])
@@ -208,57 +212,58 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
       df['Categoria Litofacie'] = categoria_lito
       df = pd.concat([df, ohe], axis = 1)
       # Criação de colunas com valor de 0 ou 1 para cada litofácie
-    
+
     if Localizacao == True:
       df = pd.concat([df, dados_lab[['X_loc', 'Y_loc', 'Z_loc', 'Caliper']]], axis = 1)
-  
+
       if Geometria == True:
         geometria = [Point(xy) for xy in zip(dados_lab['X_loc'], dados_lab['Y_loc'])]
         gdf = gpd.GeoDataFrame(dados_lab, geometry=geometria)
         gdf.set_crs(epsg=EPSG, inplace=True)
-  
+
         df['Geometria EPSG: ' + str(EPSG)] = gdf['geometry']
-  
+
         if Conversao == True:
           gdf_conv = gdf.to_crs(epsg=N_Conversao)
-          df['Geometria EPSG: ' + str(N_Conversao)] = gdf_conv['geometry'] 
+          df['Geometria EPSG: ' + str(N_Conversao)] = gdf_conv['geometry']
       # Criação da coluna com a geometria do Poço
-  
-  
+
+
     if Porosidade_i == True:
       for i in np.arange(len(distribuicao_t2)):
-          t2_transpose = pd.DataFrame([distribuicao_t2[i]]).T
-          scaler = pd.DataFrame(MaxAbsScaler().fit_transform(t2_transpose))
-          scaler_sum_phi = float(dados_lab['Porosidade RMN'][i])/float(scaler.sum())
+          t2_transpose = pd.DataFrame([distribuicao_t2[i]]).T.reset_index().drop('index', axis = 1).T
+          scaler = t2_transpose.T
+          scaler_sum_phi = float(dados_lab[str(poro_i)][i])/float(scaler.sum())
           phi_i = []
           for j in np.arange(len(scaler)):
-              p = float(scaler[0][j]*scaler_sum_phi)
+              sc = scaler.T
+              p = float(sc[j]*scaler_sum_phi)
               phi_i.append(p)
-          porosidade_i.append(list(phi_i))
+          porosi_i.append(list(phi_i))
     # Criação de uma lista de valores com distribuição de porosidade
-  
+
     if Porosidade_i == True:
-      df['Porosidade_i'] = porosidade_i
+      df['Porosidade_i'] = porosi_i
       # Criação da coluna com a porosidade
-      
+
     if T2_log == True:
-      for i in np.arange(len(porosidade_i)):
-        phi_i = porosidade_i[i]
-        tempo_log = np.log(tempo_distribuicao[i])
+      for i in np.arange(len(porosi_i)):
+        phi_i = porosi_i[i]
+        tempo_log = np.log(np.array(tempo_distribuicao[i].T.reset_index().drop('index', axis = 1), dtype = float).T[0])
         produto_porosidade_t2_log = pd.DataFrame(phi_i*tempo_log)
         sum_num = np.sum(produto_porosidade_t2_log)
         sum_den = np.sum(phi_i)
         razao_t2 = float(np.exp(sum_num/sum_den))
         media_ponderada_log.append((razao_t2))
     # Calculando o T2_log usado na função do Kenyon et al (1988)
-    
+
     if Componentes_t2 == True:
       df = pd.concat([df, dados_lab[['A_NMR', 'T2_NMR',
                                      'A1', 'T21',
                                      'A2', 'T22',
                                      'A3', 'T23']]], axis = 1)
     # Unificação dos dados com as componentes T2
-    
+
     if Fator_Cimentacao == True:
       def DefinirValor(litofacies):
         if litofacies == 'Artificial':
@@ -267,15 +272,15 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
             return V_geral
       df['Fator de Cimentacao'] = dados_lab['Litofacies'].apply(DefinirValor)
     # Incremento do valor do Fator de Cimentação
-  
+
     if Fator_Formacao == True:
       df['Fator de Formacao'] = 1/dados_lab['Porosidade RMN']**df['Fator de Cimentacao']
     # Incremento do valor do Fator de Formação
-  
+
     if Fracoes_T2Han == True:
-      for i in np.arange(len(porosidade_i)):
-        phi_i = pd.Series(porosidade_i[i])
-        porosidade = np.sum(porosidade_i[i])
+      for i in np.arange(len(porosi_i)):
+        phi_i = pd.Series(porosi_i[i])
+        porosidade = np.sum(porosi_i[i])
         a1h = phi_i[:74].sum()
         a2h = phi_i[74:84].sum()
         a3h = phi_i[84:92].sum()
@@ -284,7 +289,7 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
         phimesoh  = a2h/porosidade
         phimacroh = a3h/porosidade
         phisuperh = a4h/porosidade
-  
+
         if phimicroh <= 0.0001:
           phimicroh = 0.0001
         if phimesoh <= 0.0001:
@@ -293,78 +298,78 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
           phimacroh = 0.0001
         if phisuperh <= 0.0001:
           phisuperh = 0.0001
-  
+
         s1h.append(phimicroh)
         s2h.append(phimesoh)
         s3h.append(phimacroh)
         s4h.append(phisuperh)
-  
+
       df['S1Han'] = s1h
       df['S2Han'] = s2h
       df['S3Han'] = s3h
       df['S4Han'] = s4h
       # Cálculo das frações da modelagen Han et al (2018)
-    
+
     if Fracoes_T2Ge == True:
-      for i in np.arange(len(porosidade_i)):
-        phi_i = pd.Series(porosidade_i[i])
+      for i in np.arange(len(porosi_i)):
+        phi_i = pd.Series(porosi_i[i])
         phimicrog = phi_i[:75].sum()
         phimesog = phi_i[75:84].sum()
         phimacrog = phi_i[84:91].sum()
         phisuperg = phi_i[91:].sum()
-  
-  
+
+
         if phimicrog <= 0.0001:
                 phimicrog = 0.0001
         if phimacrog <= 0.0001:
                 phimacrog = 0.0001
         if phisuperg <= 0.0001:
                 phisuperg = 0.0001
-  
+
         s1g.append(phimicrog)
         s3g.append(phimacrog)
         s4g.append(phisuperg)
-  
+
       df['S1Ge'] = s1g
       df['S3Ge'] = s3g
       df['S4Ge'] = s4g
       # Cálculo das frações da modelagen Ge et al (2016)
-    
+
     if BVIFFI == True:
-      for i in np.arange(len(porosidade_i)):
-  
-        if dados_lab['Litofacies'][i] == 'Artifical':
-          phi_i = pd.Series(porosidade_i[i])
+      for i in np.arange(len(porosi_i)):
+
+        if tempo == 'Arenito':
+          phi_i = pd.Series(porosi_i[i])
           b = phi_i[:76].sum()
           f = phi_i[76:].sum()
-  
-  
+
+
           if b <= 0.0001:
                 b = 0.0001
           if f <= 0.0001:
                 f = 0.0001
-  
+
           BVI.append(b)
           FFI.append(f)
-  
-        else:
-          phi_i = pd.Series(porosidade_i[i])
+
+        if tempo == 'Carbonato':
+          phi_i = pd.Series(porosi_i[i])
           b = phi_i[:86].sum()
           f = phi_i[86:].sum()
-  
-  
+
+
           if b <= 0.0001:
                 b = 0.0001
           if f <= 0.0001:
                 f = 0.0001
-  
+
           BVI.append(b)
           FFI.append(f)
-  
+
       df['BVI'] = BVI
       df['FFI'] = FFI
       # Cálculo do BVI e FFI para modelagem Coates et al (1999)
-    
+
     if Dados_porosidade_Transverso == True:
       dataframe_porosidade = df['Porosidade_i']
       array_amostras = df['Amostra']
@@ -380,38 +385,38 @@ def TratamentoDadosRMN(Diretorio_pasta, Arquivo_laboratorio, Dados_niumag,
       dados_T.columns = colunas[0:N_transverso]
       df = pd.concat([df, dados_T], axis = 1)
       # Transformação da lista em colunas
-    
+
     if Amplitude == True:
       for i in np.arange(len(dados_niumag['Amplitudes'])):
         lista = []
         for j in np.arange(len(dados_niumag['Amplitudes'][i])):
           a = str(list(dados_niumag['Amplitudes'][i].reset_index().drop('index', axis = 1).T[j]))[1:-1]
           nome = "T2 " + a
-  
+
           if nome == 'T2 0.0':
               lista.append(0)
           elif nome == 'T2 10000.0':
               lista.append(df['T2 10000'][0])
           else:
               lista.append(df[nome][0])
-  
+
         if lista[0] == 0:
             lista[0] = 0.000001
         if lista[1] == 0:
             lista[1] = 0.000001
         if lista[2] == 0:
             lista[2] = 0.000001
-  
+
         A1.append(lista[0])
         A2.append(lista[1])
         A3.append(lista[2])
-  
+
       df["Amp1"] = A1
       df['Amp2'] = A2
       df['Amp3'] = A3
-  
+
     df = df.sort_values(by = 'Amostra')
-  
+
     return df
 
 
